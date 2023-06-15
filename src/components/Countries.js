26b466e73fc70275bb558072e9d81c4ff803fb31
Ogbaji Ignatius <Ogbaji.I@bullseye.local>
2023-06-15 21:03:03 +0100

    set grid display from 3 columns to 4 and also increase the bg gray from 100 to 200 and change number of default countries to display from 10 to all

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -108,11 +108,11 @@ export default function Countries() {
       </div>
 
 
-      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-      {countries.map((country) => (
-        <Article key={country.name.common} {...country} />
-      ))}
-      </div>
+     <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-4">
+        {countries.map((country) => (
+         <Article key={country.name.common} {...country} />
+        ))}
+     </div>
     
     </section>
   )}

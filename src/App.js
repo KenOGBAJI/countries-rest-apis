f1120fa36c7c7e64141ef5fc53211ad44b182de7
Ogbaji Ignatius <Ogbaji.I@bullseye.local>
2023-06-15 11:50:46 +0100

    completed the search handler and filter functionality, also remove the default filter of 10 to all 250 countrie

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Countries from "./components/Countries";
-import Error from "./components/Error";
 import SingleCountry from "./components/SingleCountry";
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Countries />}></Route>
         <Route path="/:name" element={<SingleCountry />}></Route>
-        <Route path="*" element={<Error />}></Route>
       </Routes>
     </BrowserRouter>
 
--- src/components/Countries.js
+++ src/components/Countries.js
@@ -2,14 +2,36 @@ import { useState, useEffect } from "react";
 import Article from "./Article";
 
 export default function Countries() {
-  const [countries, setCountries] = useState([])
+  const [countries, setCountries] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  const regions = [
+    {
+      name: "Europe",
+    },
+    {
+      name: "Asia",
+    },
+    {
+      name: "Africa",
+    },
+    {
+      name: "Oceania",
+    },
+    {
+      name: "Americas",
+    },
+    {
+      name: "Antarctica",
+    },
+  ]
 
   useEffect(() => {
     const getCountries = async () => {
       try {
         const res = await fetch("https://restcountries.com/v3.1/all");
         const data = await res.json();
-        setCountries(data.slice(0,10));
+        setCountries(data);
       } catch (error) {
         console.error(error);
       }
@@ -17,7 +39,35 @@ export default function Countries() {
       getCountries();
   }, []);
 
+  async function searchCountry() {
+    try {
+      const res = await fetch(`https://restcountries.com/v3.1/name/${searchText}`)
+      const data = await res.json()
+      setCountries(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
+  async function filterByRegion(region) {
+    try {
+      const res = await fetch(`https://restcountries.com/v3.1/region/${region}`)
+      const data = await res.json()
+      setCountries(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  function handleSearchCountry(e) {
+    e.preventDefault();
+    searchCountry();
+  }
+
+  function handleFilterByRegion(e) {
+    e.preventDefault();
+    filterByRegion();
+  }
   return ( 
   
   <>
@@ -26,6 +76,38 @@ export default function Countries() {
     
     <section className="container mx-auto p-8">
       {/* {form} */}
+      <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between
+      mb-8">
+        <form onSubmit={handleSearchCountry} autoComplete="off" className="max-w-4xl md:flex-1">
+          <input
+            type="search" 
+            name="search" 
+            id="search" 
+            placeholder="search for a country by it's name" 
+            required
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            className="py-3 px-4 text-gray-800 placeholder-gray-800 w-full
+            shadow-md rounded outline-none dark:text-gray-400 dark:placeholder-gray-400 dark:bg-gray-800 dark:focus:bg-gray-700 transition-all duration-200"
+            />
+        </form>
+        <form onSubmit={handleFilterByRegion}>
+          <select 
+            name="fiter-by-region" 
+            id="filter-by-region" 
+            className="w-52 py-3 px-4 outline-none shadow-md rounded text-gray-600
+            dark:text-gray-700 dark:bg-gray-700 dark:focus:bg-gray-600"
+            value={regions.name}
+            onChange={e => filterByRegion(e.target.value)}
+            >
+              {regions.map((region, index) =>(
+                <option key={index}value={region.name}>{region.name}</option>
+              ))}
+          </select>
+        </form>
+      </div>
+
+
       <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
       {countries.map((country) => (
         <Article key={country.name.common} {...country} />

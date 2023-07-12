93bdd99ff6d2c651e51b58489728e13ec0758196
Ogbaji Ignatius <Ogbaji.I@bullseye.local>
2023-07-12 14:39:01 +0100

    make few changes to text

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default function Header() {
   return (
     <>
-     <header className='flex items-center justify-between w-full uppercase font-bold rounded-md shadow-md mb-8 mt-6 fixed text-gray-800 text-md dark:text-white dark:bg-gray-700 p-10'>
+     <header className='bg-slate-200 flex items-center justify-between w-full uppercase font-bold rounded-md shadow-md mb-8 mt-6 text-gray-800 text-md dark:text-white dark:bg-gray-700 p-4'>
          <div>
            <h1>Where in the world ?</h1>
           </div>
-          <i className='fas fa moon'></i>Dark Mode
+          <i className='fas fa-moon'></i>Dark Mode
      </header>
     </>
   );
